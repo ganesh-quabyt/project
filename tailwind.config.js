7902f36ff36db0acbfa9c1168b2979062d71e449
Shreyasl340 <70383378+Shreyasl340@users.noreply.github.com>
2025-04-01 19:37:44 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["C:\Users\shreyas\Desktop\react-intern\myapp\index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
